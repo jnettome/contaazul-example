4e68bb89439c21aa0599b29daed61c6765248626
João Netto <hi@joaonetto.me>
2013-07-04 10:19:13 -0300

    Added few samples

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'json'
 gem 'contaazul', git: 'git://github.com/ateliware/contaazul'
--- web.rb
+++ web.rb
@@ -1,14 +1,45 @@
 # encoding: utf-8
 require 'sinatra'
 require 'contaazul'
+require 'json'
 
 before do
   content_type :html, 'charset' => 'utf-8'
 end
 
-get '/' do
-  Contaazul.client_secret = "00000000-0000-0000-0000-000000000000"
-  @client = Contaazul::Client.new
+enable :sessions
 
+get '/' do
+  Contaazul.external_token = ENV["CONTAAZUL_TOKEN"]
+  Contaazul.return_url = "#{request["REQUEST_URI"]}/return"
+
+  options = { :company_token => session[:company_token] } if session[:company_token]
+  @client = Contaazul::Client.new(options || {})
+
+  @is_logged_in = session[:company_token]
   erb :index
+end
+
+get '/return' do
+  session[:company_token] = params[:authorizationToken]
+
+  "<script>parent.location.reload(); window.close();</script>"
+end
+
+get '/clients' do
+  @client = Contaazul::Client.new(:company_token => session[:company_token])
+
+  @record_type = "Clients"
+  @records = JSON.parse(@client.clients)
+
+  erb :records
+end
+
+get '/clients/:id' do
+  @client = Contaazul::Client.new(:company_token => session[:company_token])
+
+  @record_type = "Client #{params[:id]}"
+  @record = @client.client(params[:id])
+
+  erb :single_record
 end
